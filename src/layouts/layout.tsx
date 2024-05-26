96468362be4290ddc8b03115c7990944872ee990
Avik-creator <avikm744@gmail.com>
2024-05-26 17:06:09 +0530

    Added Framer Motion.

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,19 +1,42 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import { motion, useInView, useAnimate, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <motion.div
+      ref={ref}
+      className="flex flex-col min-h-screen relative overflow-hidden"
+      variants={{
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      initial="hidden"
+      animate={mainControls}
+      transition={{ duration: 10, delay: 1 }}
+    >
       <Header />
       <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
-    </div>
+    </motion.div>
   );
 };
 
